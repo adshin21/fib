acb156fa3c0c1afc0ac391e42bfdd387d8c3b2dd
Aditya Kumar <adshin21@gmail.com>
2026-03-26 17:07:12 +0530

    Request ID is only generated on server side, not client intervention

--- internal/middleware/request_id.go
+++ internal/middleware/request_id.go
@@ -10,15 +10,9 @@ import (
 func RequestIDMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requestIDKey := "X-Request-ID"
-		var reqID string
-		if s := c.Request.Header.Get(requestIDKey); len(s) > 0 {
-			reqID = s
-		} else {
-			var err error
-			reqID, err = util.GetUUIDString()
-			if err != nil {
-				reqID = util.GenerateFastString(12)
-			}
+		reqID, err := util.GetUUIDString()
+		if err != nil {
+			reqID = util.GenerateFastString(12)
 		}
 		c.Set("RequestID", reqID)
 		c.Writer.Header().Set(requestIDKey, reqID)
